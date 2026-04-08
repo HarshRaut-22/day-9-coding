d41b011b0e93a7a144f58f68f708ca1dcb50b29c
HarshRaut-22 <harshraut2206@gmail.com>
2026-04-08 17:05:09 +0530

    Create array_increasing.cpp

---
+++ array_increasing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int operations =0;
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] < nums[i+1]){
+                continue;
+            }
+            else{
+                operations = operations + nums[i] + 1 -nums[i+1];
+                nums[i+1] = nums[i] + 1;
+            }
+        }
+        return operations;
+    }
+};
